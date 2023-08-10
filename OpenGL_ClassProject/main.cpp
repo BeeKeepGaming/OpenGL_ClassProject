32d2e4ebe243ae32cca4860fa11f51ac29ce7386
Eugene van den Berg <88035421+BeeKeepGaming@users.noreply.github.com>
2023-08-10 14:49:14 +0200

    End of Day (10/08/2023)

--- OpenGL_ClassProject/main.cpp
+++ OpenGL_ClassProject/main.cpp
@@ -15,7 +15,8 @@ const GLint WIDTH = 960, HEIGHT = 720;
 
 int main()
 {
-	//initialise GLFW
+	#pragma region Initialisation of a window
+	// Initialise GLFW
 	glfwInit();
 
 	//Set all required options
@@ -27,6 +28,55 @@ int main()
 
 	//Create a GLFWindow object that can use for GLFW's functions
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "MyWindowName", nullptr, nullptr);
+
+	//Actual screen resolution
+	int screenW, screenH;
+
+	//Get screen resolution
+	glfwGetFramebufferSize(window, &screenW, &screenH);
+
+	//Check if window was created successfully
+	if (nullptr == window) {
+		cout << "Failed to create window." << endl;
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	//Making the window
+	glfwMakeContextCurrent(window);
+
+	//Enable GLEW
+	glewExperimental = GL_TRUE;
+
+	//Initialise GLEW
+	if (GLEW_OK != glewInit()) {
+		cout << "Failed to initialise GLEW." << endl;
+		return EXIT_FAILURE;
+	}
+
+	//Setup OpenGL viewport
+	//Define viewport dimentions
+	glViewport(0, 0, screenW, screenH);
+
+	//Game loop
+	while (!glfwWindowShouldClose(window)) {
+		//Checks for events and calls corresponding response
+		glfwPollEvents();
+
+		//Render
+		//Clear the colour buffer
+		glClearColor(0.4f, 0.8f, 0.4f, 1.0f);
+		//6.0f, 226.0f, 109.0f
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Draw the OpenGL window/viewport
+		glfwSwapBuffers(window);
+	}
+
+	//Terminate GLFW and clear any resources from GLFW
+	glfwTerminate();
+
+	#pragma endregion
 
 	return EXIT_SUCCESS;
 }
