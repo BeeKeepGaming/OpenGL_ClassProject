11df1d4cd03d29d8322eec057a68d6a91e717050
Eugene van den Berg <88035421+BeeKeepGaming@users.noreply.github.com>
2023-08-10 08:45:54 +0200

    Add project files.

---
+++ OpenGL_ClassProject/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+//GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+//GLFW
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	return EXIT_SUCCESS;
+}
