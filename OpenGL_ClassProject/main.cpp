12c32ab47c29fca43f4392870419d6555b3ebae8
Eugene van den Berg <88035421+BeeKeepGaming@users.noreply.github.com>
2023-08-10 09:02:09 +0200

    Initialising openGL window

--- OpenGL_ClassProject/main.cpp
+++ OpenGL_ClassProject/main.cpp
@@ -10,7 +10,23 @@ using namespace std;
 //GLFW
 #include <GLFW/glfw3.h>
 
+//Window Dimention
+const GLint WIDTH = 960, HEIGHT = 720;
+
 int main()
 {
+	//initialise GLFW
+	glfwInit();
+
+	//Set all required options
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	//Create a GLFWindow object that can use for GLFW's functions
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "MyWindowName", nullptr, nullptr);
+
 	return EXIT_SUCCESS;
 }
