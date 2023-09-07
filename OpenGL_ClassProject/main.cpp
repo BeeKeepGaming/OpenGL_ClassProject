c79a64c40523ef6c8794a70a2e04442bc1db24ad
Eugene van den Berg <88035421+BeeKeepGaming@users.noreply.github.com>
2023-09-07 20:33:36 +0200

    End of day (07/09/2023)

--- OpenGL_ClassProject/main.cpp
+++ OpenGL_ClassProject/main.cpp
@@ -21,8 +21,29 @@ using namespace std;
 //Link Shader File
 #include "Shader.h"
 
+//Link Camera File
+#include "Camera.h"
+
 //Window Dimention
 const GLint WIDTH = 960, HEIGHT = 720;
+int SCREEN_WIDTH, SCREEN_HEIGHT; // Replace all screenW & screenH with these
+
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void ScrollCallback(GLFWwindow* window, double xOffset, double yOffset);
+void MouseCallback(GLFWwindow* window, double xPos, double yPos); // Get mouse pos in order to hide it
+void ProcessInput(GLFWwindow* window); // Move camera
+
+// Set Initial values for camera ***
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+GLfloat lastX = WIDTH / 2.0f;
+GLfloat lastY = HEIGHT / 2.0f;
+bool keys[1024]; // Array of 1024 different types of keys
+bool firstMouse = true; // Only handling one type of mouse, thus true
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
+
+//Switch Cameras
+bool camLocked = false;
 
 int main()
 {
@@ -40,11 +61,8 @@ int main()
 	//Create a GLFWindow object that can use for GLFW's functions
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "MyWindowName", nullptr, nullptr);
 
-	//Actual screen resolution
-	int screenW, screenH;
-
 	//Get screen resolution
-	glfwGetFramebufferSize(window, &screenW, &screenH);
+	glfwGetFramebufferSize(window, &SCREEN_WIDTH, &SCREEN_HEIGHT);
 
 	//Check if window was created successfully
 	if (nullptr == window) {
@@ -55,6 +73,14 @@ int main()
 
 	//Making the window
 	glfwMakeContextCurrent(window);
+
+    // Set the required callback functions
+    glfwSetKeyCallback(window, KeyCallback);
+    glfwSetCursorPosCallback(window, MouseCallback);
+    glfwSetScrollCallback(window, ScrollCallback);
+
+    // Center cursor to window and Hide the cursor for a more immersive experience
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	//Enable GLEW
 	glewExperimental = GL_TRUE;
@@ -67,7 +93,7 @@ int main()
 
 	//Setup OpenGL viewport
 	//Define viewport dimentions
-	glViewport(0, 0, screenW, screenH);
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	// Enable depth in the project
 	glEnable(GL_DEPTH_TEST);
@@ -75,6 +101,9 @@ int main()
 	// Enable alpha support for images
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+#pragma endregion
+
+#pragma region Build Shader - Cubes
 
 	//Build & Compile Shader Program
 	Shader ourShader("core.vs", "core.frag");
@@ -134,6 +163,21 @@ int main()
 		-0.5f, -0.5f, -0.5f,		0.0f, 0.0f
 	};
 
+    // Positions of different cubes
+    glm::vec3 cubePositions[] =
+    {
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),
+        glm::vec3(-1.3f, 1.0f, -1.5f)
+    };
+
 	// Generate the vertex arrays and vertex buffers and save them into variables
 	GLuint VBA, VOA;
 	glGenVertexArrays(1, &VOA);
@@ -184,58 +228,183 @@ int main()
 		glBindTexture(GL_TEXTURE_2D, 0);
 
 	#pragma endregion
+#pragma endregion
 
-		// Create Projection Matrix
-		glm::mat4 projection(1.0f);
-		//Perspective view
-		projection = glm::perspective(glm::radians(45.0f), (GLfloat)screenW / (GLfloat)screenH, 0.1f, 100.0f);
-		//Orthographic view
-		//projection = glm::ortho(0.0f, (GLfloat)screenW, 0.0f, (GLfloat)screenH, 0.1f, 100.0f);
+#pragma region Build Shader - Height Map
+        Shader shaderHM("coreHM.vs", "coreHM.frag");
+
+        // Create and load texture replace it with your own image path.
+        int widthHM, heightHM, nrChannelsHM;
+
+        unsigned char* dataHM = SOIL_load_image("res/images/Hight_Map.png", &widthHM, &heightHM, &nrChannelsHM, 0);
+
+        if(dataHM)
+        {
+            cout << "Loaded heightmap of size " << heightHM << " x " << widthHM << endl;
+        }
+        else
+        {
+            cout << "Failed to load texture" << endl;
+        }
+
+        // set up vertex data (and buffer(s)) and configure vertex attributes
+        vector<GLfloat> verticesHM;
+        GLfloat yScale = 0.1; //normalize the height map data and scale it to the desired height [0f : 64.0f]
+        GLfloat yShift = 16.0f; // translate the elevations to our final desired range [-16.0f : 48.0f]
+        int rez = 1;
+        GLuint bytePerPixel = nrChannelsHM;
+
+        for (int i = 0; i < heightHM; i++)
+        {
+            for (int j = 0; j < widthHM; j++)
+            {
+                unsigned char* pixelOffset = dataHM + (j + widthHM * i) * bytePerPixel;
+                unsigned char y = pixelOffset[0];
+
+                // vertex
+                verticesHM.push_back(-heightHM / 2.0f + heightHM * i / (float)heightHM); // vx
+                verticesHM.push_back((int)y * yScale - yShift); // vy
+                verticesHM.push_back(-widthHM / 2.0f + widthHM * j / (float)widthHM); // vz
+            }
+        }
+        cout << "Loaded " << verticesHM.size() / 3 << " vertices" << endl;
+        SOIL_free_image_data(dataHM);
+
+        vector<GLuint> indicesHM;
+        for (int i = 0; i < heightHM - 1; i += rez)
+        {
+            for (int j = 0; j < widthHM; j += rez)
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    indicesHM.push_back(j + widthHM * (i + k * rez));
+                }
+            }
+        }
+        cout << "Loaded " << indicesHM.size() << " indices" << endl;
+
+        const int numStrips = (heightHM - 1) / rez;
+        const int numTrisPerStrip = (widthHM / rez) * 2 - 2;
+        cout << "Created lattice of " << numStrips << " strips with " << numTrisPerStrip << " triangles each" << endl;
+        cout << "Created " << numStrips * numTrisPerStrip << " triangles total" << endl;
+
+        // Generate the vertex arrays, vertex buffers and index buffers and save them into variables
+        unsigned int VAO, VBO, IBO;
+        glGenVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
+
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, verticesHM.size() * sizeof(float), &verticesHM[0], GL_STATIC_DRAW);
+
+        // Position attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glGenBuffers(1, &IBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesHM.size() * sizeof(unsigned), &indicesHM[0], GL_STATIC_DRAW);
 
 #pragma endregion
 
 	//Game loop
 	while (!glfwWindowShouldClose(window)) {
+        // Set frame time
+        GLfloat currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
 		//Checks for events and calls corresponding response
 		glfwPollEvents();
+
+        //Handle the movement 
+        ProcessInput(window);
 
 		//Render
 		//Clear the colour buffer
 		glClearColor(0.1f, 0.3f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+#pragma region Draw Cube
+
 		// DRAW OUR CUBE		
 		ourShader.Use();
-
-		// CREATE CUBE *
-		glm::mat4 model(1.0f);
-		glm::mat4 view(1.0f);
-		//Perspective view
-		model = glm::rotate(model, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.5f, 1.0f, 0.0f)); //Perspective view
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); //Perspective view
-		//Orthographic view
-		//model = glm::rotate(model, 0.5f, glm::vec3(1.0f, 0.0f, 0.0f)); //Orthographic view
-		//view = glm::translate(view, glm::vec3(screenW/2, screenH/2, -700.0f)); //Orthographic view
-
-		// Get the uniform locations for our matrices
-		GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
-		GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
-		GLint projLoc = glGetUniformLocation(ourShader.Program, "projection");
-
-		// Pass locations to shaders ***
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
-		// Draw container
-		glBindVertexArray(VOA);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		glBindVertexArray(0); // Unbinding
 
 		// Activate specified texture
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture);
 		glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture1"), 0);
+
+        // Create Projection Matrix - (moved into while loop in order to update zoom)
+        glm::mat4 projection(1.0f);
+        //Perspective view
+        projection = glm::perspective(camera.GetZoom(), (GLfloat)SCREEN_WIDTH / (GLfloat)SCREEN_HEIGHT, 0.1f, 100.0f);
+
+		/// Create camera transformation
+        glm::mat4 view(1.0f);
+        view = camera.GetViewMatrix();
+
+        // Get the uniform locations for our matrices
+        GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
+        GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
+        GLint projLoc = glGetUniformLocation(ourShader.Program, "projection");
+
+        // Pass locations to shaders
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		// Draw container
+		glBindVertexArray(VOA);
+		
+        for (GLuint i = 0; i < 10; i++)
+        {
+            // Calculate the model matrix for each object and pass it to the shader before drawing
+            glm::mat4 model(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            GLfloat angle = 20.0f * i;
+            model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        glBindVertexArray(0); // Unbinding
+#pragma endregion
+
+#pragma region Draw HightMap
+        // Activate Shader
+        shaderHM.Use();
+
+        // view/projection transformations
+        glm::mat4 projectionHM = glm::perspective(glm::radians(camera.GetZoom()), (float)WIDTH / (float)HEIGHT, 0.1f, 100000.0f);
+        glm::mat4 viewHM = camera.GetViewMatrix();
+        GLint projLocHM = glGetUniformLocation(shaderHM.Program, "projection");
+        GLint viewLocHM = glGetUniformLocation(shaderHM.Program, "view");
+
+        glUniformMatrix4fv(viewLocHM, 1, GL_FALSE, glm::value_ptr(viewHM));
+        glUniformMatrix4fv(projLocHM, 1, GL_FALSE, glm::value_ptr(projectionHM));
+
+        // world transformation
+        glm::mat4 modelHM = glm::mat4(1.0f);
+        GLint modelLocHM = glGetUniformLocation(shaderHM.Program, "model");
+        glUniformMatrix4fv(modelLocHM, 1, GL_FALSE, glm::value_ptr(modelHM));
+
+        // Draw container
+        glBindVertexArray(VAO);
+
+        for (int strip = 0; strip < numStrips; strip++)
+        {
+            glDrawElements(GL_TRIANGLE_STRIP, // primitive type
+                numTrisPerStrip + 2, // number of indices to render
+                GL_UNSIGNED_INT, // index data type
+                (void*)(sizeof(GLuint) * (numTrisPerStrip + 2) * strip)); // offset to starting index
+        }
+
+        glBindVertexArray(0); // Unbinding
+
+
+#pragma endregion
+
 
 		//Draw the OpenGL window/viewport
 		glfwSwapBuffers(window);
@@ -249,4 +418,108 @@ int main()
 	glfwTerminate();
 
 	return EXIT_SUCCESS;
+}
+
+// Moves/alters the camera positions based on user input
+// WASD and Arrow keys
+void ProcessInput(GLFWwindow* window)
+{
+    //Free Camera controls
+    if (!camLocked) {
+        if (keys[GLFW_KEY_W])
+        {
+            camera.ProcessKeyboard(FORWARD, deltaTime);
+        }
+
+        if (keys[GLFW_KEY_S])
+        {
+            camera.ProcessKeyboard(BACKWARD, deltaTime);
+        }
+
+        if (keys[GLFW_KEY_A])
+        {
+            camera.ProcessKeyboard(LEFT, deltaTime);
+        }
+
+        if (keys[GLFW_KEY_D])
+        {
+            camera.ProcessKeyboard(RIGHT, deltaTime);
+        }
+    }
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int modifiers)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    if (key == GLFW_KEY_TAB && action == GLFW_PRESS)
+    {
+        if (camLocked == true)
+        {
+            camLocked = false;
+        }
+        else
+        {
+            camLocked = true;
+        }
+    }
+
+    //Cycle Camera Left
+    if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
+    {
+        camLocked = true;
+        camera.CycleCamera("Left");
+    }
+
+    //Cycle Camera Right
+    if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+    {
+        camLocked = true;
+        camera.CycleCamera("Right");
+    }
+
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+        {
+            keys[key] = true;
+        }
+        else if (action == GLFW_RELEASE)
+        {
+            keys[key] = false;
+        }
+    }
+}
+
+// GLFW: whenever the mouse moves, this callback is called
+void MouseCallback(GLFWwindow* window, double xPos, double yPos)
+{
+    if (!camLocked) {
+        if (firstMouse)
+        {
+            lastX = xPos;
+            lastY = yPos;
+            firstMouse = false;
+        }
+
+        GLfloat xOffset = xPos - lastX;
+        GLfloat yOffset = lastY - yPos;  // Reversed since y-coordinates go from bottom to left
+
+        lastX = xPos;
+        lastY = yPos;
+
+        camera.ProcessMouseMovement(xOffset, yOffset);
+    }
+}
+
+// GLFW: whenever the mouse scroll wheel scrolls, this callback is called
+void ScrollCallback(GLFWwindow* window, double xOffset, double yOffset)
+{
+    if (!camLocked) {
+        camera.ProcessMouseScroll(yOffset);
+    }
 }
